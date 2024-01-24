05a618400c28bb09addb516a5b649c42907a827b
Leonardo Ascione <112330494+rockfactory@users.noreply.github.com>
2024-01-24 03:32:35 +0100

    Add check for invalid planets

--- src/WayfarersWings/Models/Configs/Template/WingTemplateConfigCelestialBody.cs
+++ src/WayfarersWings/Models/Configs/Template/WingTemplateConfigCelestialBody.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using KSP.Sim.impl;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
 namespace WayfarersWings.Models.Configs.Template;
@@ -19,4 +20,29 @@ public enum CelestialBodyVariantFlag
 public class WingTemplateConfigCelestialBody
 {
     public List<CelestialBodyVariantFlag> requires = [];
+
+    /// <summary>
+    /// Some wings are only valid for certain celestial bodies.
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public bool IsValid(CelestialBodyComponent celestialBody)
+    {
+        foreach (var requirement in requires)
+        {
+            var isValid = requirement switch
+            {
+                CelestialBodyVariantFlag.HasSurface => celestialBody.hasSolidSurface,
+                CelestialBodyVariantFlag.HasAtmosphere => celestialBody.hasAtmosphere,
+                CelestialBodyVariantFlag.HasOcean => celestialBody.hasOcean,
+                CelestialBodyVariantFlag.IsMoon => celestialBody.referenceBody?.IsStar == false,
+                CelestialBodyVariantFlag.IsPlanet => celestialBody.referenceBody?.IsStar == true,
+                CelestialBodyVariantFlag.IsStar => celestialBody.IsStar,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            if (!isValid) return false;
+        }
+
+        return true;
+    }
 }
